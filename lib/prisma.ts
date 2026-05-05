21fb2bb585870aeb9a86b06d3ad949b1b9492a1b
bruno mundim <brunorm@gmx.com>
2026-05-05 13:31:49 -0300

    página inicial

---
+++ lib/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
+export default prisma
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+// export default prisma
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
