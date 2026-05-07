b98d5008ad60824f513992a06e868690025b68ea
bruno mundim <brunorm@gmx.com>
2026-05-07 13:12:04 -0300

    foce-dynamic

--- lib/data.ts
+++ lib/data.ts
@@ -1,16 +1,20 @@
 import postgres from "postgres";
+
 import { Post } from "./definitions";
 //prepare: false, para eliminar cache e não dar imcompatibilidade de tipos quando houver
 // alguma alteração em alguma tabela do banco de dados.
 
+export const dynamic = 'force-dynamic' 
+
 const sql = postgres(process.env.DATABASE_URL!, {
   ssl: "require",
-  prepare: false
+  prepare: false,
 });
 
 export async function listPosts() {
   const data = await sql<Post[]>`
     SELECT * FROM "Post"
   `;
+
   return data;
 }
